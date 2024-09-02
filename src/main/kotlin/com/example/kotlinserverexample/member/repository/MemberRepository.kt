6c8b3ddec6d0f1129c65f2dd5bc80ee08196f583
Y-jaeheon <121541876+Y-jae-heon@users.noreply.github.com>
2024-09-02 14:33:09 +0900

    Revert "init: 초기 설정"
    This reverts commit bb8f1915a2537bebcac3ea6bc10be9f89a0ae251.

--- src/main/kotlin/com/example/kotlinserverexample/member/repository/MemberRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kotlinserverexample.member.repository
-
-import com.example.kotlinserverexample.member.entity.MemberEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface MemberRepository: JpaRepository<MemberEntity, Long> {
-}
